7976495187334ebcdcef16141ee2b41e029b62bc
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-05-22 10:30:56 +0530

    added base route

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const PORT = process.env.PORT || 3000;
 const userRoutes = require('./routes/userRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
 
+app.get('/', async (req,res)=>{
+    try {
+
+        res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Voting-Api` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
 // Use the routers
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
